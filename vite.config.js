f2c06d6c53b3ccfa7cb19e8a616984f0c8e88a4c
Debabrata Dutta <debabrata.dutta7777@gmail.com>
2026-03-12 03:14:53 +0530

    deploy new changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/my_portfolio_react/',
   build: {
     outDir: 'dist',
     sourcemap: false,
